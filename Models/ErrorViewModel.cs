808a0a2ba526046f4c2daf48beed618260dd6de3
Luis Mesa <luismesagravedeperalta@gmail.com>
2020-08-08 12:11:24 +0200

    app up and running ok

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bustroker.AnonymousWhatsapp.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
